549f5a5977f726f826dc9ff8d92eece9e6441aa1
benjamam <benjamam@miamioh.edu>
2017-07-20 12:05:24 -0400

    successsful food api post, table generated

--- main.js
+++ main.js
@@ -41,9 +41,54 @@ function itemLookup(ndbno){
     var uri = baseUri + "reports/?ndbno=" + ndbno + "&api_key=" + apiKey + "&" + type + "&" + format;
     $.ajax({
         url: uri,
+        contentType: 'application/json',
+        dataType: 'json',
         type: 'post',
         success: function(data){
             console.log(data);
+            var food = data.report.food;
+            var name = food.name;
+            console.log(food);
+            var nutrients = food.nutrients;
+            var vitA = "318";
+            var energy = "208"; //kcal
+            var protein = "203";
+            var fat = "204";
+            var carb = "205";
+            var fiber = "291";
+            var sugar = "269";
+            var vitC = "401";
+            var vitB6 = "415";
+            var vitB12 = "418";
+            var vitD = "324";
+            var relevantMetrics = [energy, carb, protein, fat, sugar, fiber, vitA, vitB6, vitB12, vitC, vitD];
+            $('#results').html('<br/><h3>' + name + '</h3>');
+            var table = "<table class='table table-inverse'>";
+            table += "<tbody>";
+            
+            for(var i=0; i<nutrients.length; i++){
+                //console.log(i + ": " + food.nutrients[i]);
+                var curNutr = nutrients[i];
+                for(var j=0; j<relevantMetrics.length; j++){
+                    if(curNutr.nutrient_id == relevantMetrics[j]){
+                        table += "<tr>";
+                        table += "<td>" + nutrients[i].name + "</td>";
+                        table += "<td>" + nutrients[i].value + "</td>";
+                        table += "<td>" + nutrients[i].unit + "</td>";
+                        table += "</tr>";    
+                    }
+                }
+                /*if(nutrients[i].nutrient_id == vitA){
+                    console.log(nutrients[i].name + ": " + nutrients[i].value);
+                    table += "<td>" + nutrients[i].name + "</td>";
+                    table += "<td>" + nutrients[i].value + "</td>";
+                    table += "<td>" + nutrients[i].unit + "</td>";
+                } */
+            }
+            
+            table += "</tbody>";
+            table += "</table>";
+            $('#results').append(table);
         }
     }) 
 }
